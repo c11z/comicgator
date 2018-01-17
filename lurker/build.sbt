92327155627256f911cf5240c4fca16ba272a689
Cory Dominguez <corydominguez@gmail.com>
2018-01-16 21:34:28 -0800

    Small refinements for docker-compose setup

--- lurker/build.sbt
+++ lurker/build.sbt
@@ -1,21 +1,21 @@
 name := "lurker"
 
 scalaVersion := "2.12.3"
-version := "0.3-SNAPSHOT"
+version := "0.3.4-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(JavaAppPackaging, DockerPlugin)
 
 libraryDependencies ++= Seq(
-  "com.typesafe.play" %% "play-json" % "2.6.3",
+  "com.typesafe.play" %% "play-json" % "2.6.8",
   "com.typesafe.slick" %% "slick" % "3.2.1",
   "com.typesafe.slick" %% "slick-hikaricp" % "3.2.1",
-  "org.seleniumhq.selenium" % "selenium-java" % "3.6.0",
-  "org.seleniumhq.selenium" % "htmlunit-driver" % "2.27",
+  "org.seleniumhq.selenium" % "selenium-java" % "3.8.1",
+  "org.seleniumhq.selenium" % "htmlunit-driver" % "2.29.0",
   "org.postgresql" % "postgresql" % "42.1.4",
-  "org.mongodb" % "bson" % "3.2.2",
+  "org.mongodb" % "bson" % "3.6.1",
   "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2",
   "ch.qos.logback" % "logback-classic" % "1.2.3",
-  "com.google.cloud" % "google-cloud-storage" % "1.6.0"
+  "com.google.cloud" % "google-cloud-storage" % "1.14.0"
 )
 
 fork := true
--- lurker/src/main/scala/com/comicgator/lurker/Main.scala
+++ lurker/src/main/scala/com/comicgator/lurker/Main.scala
@@ -10,46 +10,53 @@ import scala.io.Source
 object Main extends App with Conf with LazyLogging {
   // Use single execution context for future sync.
   implicit val ec: ExecutionContext = ExecutionContext.global
-  // Load comic configuration from json file.
-  private val comics: Vector[Comic] = Json
-    .fromJson[Vector[Comic]](
-      Json.parse(Source.fromResource("comics.json").getLines().mkString))
-    .get
+  logger.info("Waiting on you to initialize the database...")
+  Thread.sleep(7 * 60 * 1000) // 7 minutes
 
-  // Launch etl process.
-  // Await sequenced future results before shutting down.
-  Await.result(Mech.etl(comics), Duration.Inf)
+  while (true) {
+    // Load comic configuration from json file.
+    val comics: Vector[Comic] =
+      Json
+        .fromJson[Vector[Comic]](
+        Json.parse(Source.fromResource("comics.json").getLines().mkString))
+        .get
 
-  // Run Maintenance queries to iterate feeds and refresh views
-  val maintenance: Future[Int] = for {
-    replay <- Repository.replayFeedStrip
-    latest <- Repository.latestFeedStrip
-  } yield {
-    val successCount = replay + latest
-    logger.info(s"Ran $successCount maintenance queries.")
-    successCount
-  }
-  Await.result(maintenance, Duration.Inf)
+    // Launch etl process.
+    // Await sequenced future results before shutting down.
+    Await.result(Mech.etl(comics), Duration.Inf)
 
-  // Generate updated RSS feeds and send to Google Storage
-  val feedSuccess: Future[Boolean] = {
-    Repository.readyFeeds.map { (allItems: Vector[Item]) =>
-      allItems
-        .groupBy(item => item.feedId)
-        .map {
-          case (feedId, items) =>
-            val feedXML = Syndication.makeFeed(items)
-            val blob = Storage.putFeed(feedId, feedXML)
-            logger.info(
-              s"http://$FEED_STORAGE_BUCKET/${feedId.toString}/rss.xml")
-            blob.exists()
-        }
-        .forall(identity)
+    // Run Maintenance queries to iterate feeds and refresh views
+    val maintenance: Future[Int] = for {
+      replay <- Repository.replayFeedStrip
+      latest <- Repository.latestFeedStrip
+    } yield {
+      val successCount = replay + latest
+      logger.info(s"Ran $successCount maintenance queries.")
+      successCount
     }
+    Await.result(maintenance, Duration.Inf)
+
+    // Generate updated RSS feeds and send to Google Storage
+    val feedSuccess: Future[Boolean] = {
+      Repository.readyFeeds.map { (allItems: Vector[Item]) =>
+        allItems
+          .groupBy(item => item.feedId)
+          .map {
+            case (feedId, items) =>
+              val feedXML = Syndication.makeFeed(items)
+              val blob = Storage.putFeed(feedId, feedXML)
+              logger.info(
+                s"http://$FEED_STORAGE_BUCKET/${feedId.toString}/rss.xml")
+              blob.exists()
+          }
+          .forall(identity)
+      }
+    }
+
+    Await.result(feedSuccess, Duration.Inf)
+    logger.info("Finished Loop")
+    Thread.sleep(10 * 60 * 1000) // 10 minutes
   }
-
-  Await.result(feedSuccess, Duration.Inf)
-
   // Safely close database connection pool.
   Repository.shutdown()
   // Exit successfully.
