0f897af135011f1fadc55984b59a2e3c147d9d84
Cory Dominguez <corydominguez@gmail.com>
2018-01-16 21:32:08 -0800

    remove extra timestamp zone stuff

---
+++ cdb/integration.sql
@@ -0,0 +1,3 @@
+insert into geek (id, email) values ('5a5e2c56ccffe40672039cc4', 'corydominguez@gmail.com');
+insert into feed (id, geek_id, name) values ('5a5e2cc91590ad06721bfe0f', '5a5e2c56ccffe40672039cc4', 'SMBC replay');
+insert into feed_comic (feed_id, comic_id, is_replay, mark, step) values ('5a5e2cc91590ad06721bfe0f', '5734142bde7af93a83733281', true, 0, 10);
